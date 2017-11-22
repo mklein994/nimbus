ef17debfd33c08c46e795d4a1bcfec994fa22a8c
Matthew Klein <mklein994@gmail.com>
2017-11-21 20:40:40 -0600

    Add command line arguments

---
+++ src/args.rs
@@ -0,0 +1,67 @@
+//#![cfg_attr(feature = "clippy", plugin(clippy))]
+//#![cfg_attr(feature = "clippy", warn(clippy_pedantic))]
+//#![cfg_attr(feature = "clippy", allow(missing_docs_in_private_items))]
+
+use clap::{App, Arg};
+
+const LOCATION_HELP: &'static str = "\
+Latitude and longitude (in decimal degrees), separated by a colon.
+Latitude: Positive is north, negative is south.
+Longitude: Positive is east, negative is west.";
+
+const UNITS_HELP: &'static str = "\
+\u{2218} auto: automatically select units based on geographic location
+
+\u{2218} ca: same as si, except that wind speed and wind gust are in kilometers per hour
+
+\u{2218} uk2: same as si, except that nearest storm distance and visibility are in miles, and wind \
+speed and wind gust are in miles per hour
+
+\u{2218} us: imperial units
+
+\u{2218} si: SI units";
+
+pub fn build_cli() -> App<'static, 'static> {
+    App::new("weather")
+        .about(crate_description!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .arg(
+            Arg::with_name("config")
+                .empty_values(false)
+                .short("c")
+                .long("config")
+                .number_of_values(1)
+                .help("Path to configuration")
+                .long_help("Path to configuration. Follows the XDG Base Directory Specification."),
+        )
+        .arg(
+            Arg::with_name("location")
+                .short("l")
+                .long("location")
+                .value_names(&["LAT", "LON"])
+                .value_delimiter(":")
+                .require_delimiter(true)
+                .help("Latitude and longitude (in decimal degrees), separated by a colon")
+                .long_help(LOCATION_HELP),
+        )
+        .arg(
+            Arg::with_name("language")
+                .long("language")
+                .short("g")
+                .default_value("en")
+                .help("Language to show results in.")
+                .long_help(
+                    "Supported values come from <https://darksky.net/dev/docs#request-parameters>.",
+                ),
+        )
+        .arg(
+            Arg::with_name("units")
+                .long("units")
+                .short("u")
+                .possible_values(&["auto", "ca", "uk2", "us", "si"])
+                .default_value("auto")
+                .help("Unit display weather conditions will be shown in.")
+                .long_help(UNITS_HELP),
+        )
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 #![cfg_attr(feature = "clippy", warn(clippy_pedantic))]
 #![cfg_attr(feature = "clippy", allow(missing_docs_in_private_items))]
 
+#[macro_use]
+extern crate clap;
 extern crate darksky;
 extern crate dotenv;
 #[macro_use]
@@ -13,28 +15,28 @@ extern crate serde_derive;
 extern crate toml;
 extern crate nimbus;
 
-mod config;
-
 use config::Config;
 
+mod config;
+mod args;
+
 fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<::std::error::Error>> {
     pretty_env_logger::init().expect("Failed to initialize pretty_env_logger");
     info!("Starting up\u{2026}");
 
+    let m = args::build_cli().get_matches();
+    info!("{:#?}", m);
+
     info!("Retrieving settings\u{2026}");
+    let config: Config = Config::load()?;
+    info!("{:#?}", config);
 
-    let config = match Config::load() {
-        Ok(c) => c,
-        Err(e) => panic!("error getting config: {:?}", e),
-    };
-
-    info!("{:?}", config);
-    // NOTE: END HERE
-
-    /*
-    if let Err(e) = nimbus::run(config) {
-        eprintln!("Application error: {}", e);
-        std::process::exit(1);
-    }
-    */
+    Ok(())
 }
