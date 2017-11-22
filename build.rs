238b684872642f6110be01ec01de493ad2b59c08
Matthew Klein <mklein994@gmail.com>
2017-11-21 20:44:53 -0600

    Build shell completion with a build script

---
+++ build.rs
@@ -0,0 +1,19 @@
+//#![cfg_attr(feature = "clippy", feature(plugin))]
+
+#[macro_use]
+extern crate clap;
+
+use clap::Shell;
+
+include!("src/args.rs");
+
+fn main() {
+    let outdir = match ::std::env::var_os("OUT_DIR") {
+        None => return,
+        Some(outdir) => outdir,
+    };
+    println!("{:?}", outdir);
+
+    let mut app = build_cli();
+    app.gen_completions(crate_name!(), Shell::Bash, &outdir)
+}
